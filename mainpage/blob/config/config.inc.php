f62259f9982c404990ea79ae1ed2b36ecb191ce9
Chirath <chirath.02@gmail.com>
2015-12-10 10:41:47 +0530

    djvndvn

---
+++ mainpage/blob/config/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+
+# Database management system to use Currently, we support only MySQL. Will try to provide support for other databases in the future
+
+$DBMS = 'MySQL';
+
+# Database variables
+
+$_BLOB = array();
+$_BLOB[ 'db_server' ] = 'localhost';
+$_BLOB[ 'db_database' ] = 'blob';
+$_BLOB[ 'db_user' ] = 'root';
+$_BLOB[ 'db_password' ] = '1996';
+
+?>
